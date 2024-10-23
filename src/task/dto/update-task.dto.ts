de6efcb5b3125f58b82284a59823fd4b818a9270
Jakub Lewandowski <lewando54@gmail.com>
2024-10-23 16:23:39 +0200

    Make update-task.dto fields optional

--- src/task/dto/update-task.dto.ts
+++ src/task/dto/update-task.dto.ts
@@ -1,20 +1,25 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateTaskDto } from './create-task.dto';
 import { Status } from '@prisma/client';
-import { IsString, IsEnum } from 'class-validator';
+import { IsString, IsEnum, IsOptional } from 'class-validator';
 import { ApiProperty } from '@nestjs/swagger';
 
 export class UpdateTaskDto extends PartialType(CreateTaskDto) {
+  @IsOptional()
   @IsString()
   @ApiProperty({
     type: String,
   })
-  title: string;
+  title?: string;
+
+  @IsOptional()
   @IsString()
   @ApiProperty({
     type: String,
   })
   description?: string;
+
+  @IsOptional()
   @IsEnum(Status)
   @ApiProperty({
     type: String,
